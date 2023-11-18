12b34b14c2c7f5d4bd1b939fae3830efa54ced9a
Stephanie Marie <fullstackwebdevsteph@gmail.com>
2023-11-18 12:56:54 -0400

    started files / first alert and promp js

---
+++ script.js
@@ -0,0 +1,24 @@
+//1.Ask user if they want to play RPS to initiate game - prompt?
+//Use confirm() <- This is a function with ok or cancel message and button.
+//Start by storing the users response in a variable.
+var userWantsToPlay = confirm('Do you want to lay Rock, Paper, Scissors?');
+
+if (userWantsToPlay) {
+  alert(
+    "I do too! It's simple, here's what you do: Press 'R' for Rock, Press 'P' for Paper and press 'S' for scissors. Lets start! "
+  );
+} else alert('Next time then!');
+
+//2.Tell user the rules; press R for rock, P for paper and S scissors
+//Get their choice - prompt
+
+//3.Figure out how to generate random computer choice
+//Display computers choice - alert
+
+//4. Compare choices - if else?
+//Display winner - alert
+
+//5.Store result and keep track of running score win/loss/tie ?
+//something about
+
+//6. Play again? - prompt
